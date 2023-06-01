2cec3557c87f426550a876543a8232cb5b98edda
Heng Lu <79895375+ms-henglu@users.noreply.github.com>
2023-06-01 14:58:03 +0800

    support data source (#59)

---
+++ CHANGELOG.md
@@ -0,0 +1,76 @@
+## v0.10.0
+
+FEATURES:
+- Support generate data source from Swagger GET example.
+- Support generate dependency automatically for identity.
+
+ENHANCEMENTS:
+
+BUG FIXES:
+- Fix error message in reports.
+
+## v0.9.0
+
+ENHANCEMENTS:
+- Update mapping of azurerm v3.50.0
+
+## v0.8.1
+
+BUG FIXES:
+- Fix invalid characters in folder name on Windows
+
+## v0.8.0
+
+FEATURES:
+- Support error report
+
+ENHANCEMENTS:
+- Update mapping of azurerm v3.41.0
+
+## v0.7.0
+
+FEATURES:
+- Generate a passed report or partially passed report
+
+ENHANCEMENTS:
+- Update mapping of azurerm v3.30.0
+
+## v0.6.0
+
+BUG FIXES:
+- Dependency detection failed when working-dir is specified
+
+## v0.5.0
+
+ENHANCEMENTS:
+- Update mapping of azurerm v3.24.0
+
+BUG FIXES:
+- Wrap keys which start with numbers.
+
+## v0.4.0
+
+ENHANCEMENTS:
+- Update mapping of azurerm v3.22.0
+
+## v0.3.0
+
+FEATURES:
+- Supports validate command to preview the resource changes.
+- Supports -working-dir option to specify the working directory.
+
+## v0.2.1
+
+FEATURES:
+- Generated document improvement: now differences are highlighted.
+
+## v0.2.0
+
+FEATURES:
+- Support install Terraform automatically.
+- Support `-raw` option, which allows user to use raw json payload. The default payload will use jsonencode function.
+- Support `-overwrite` option, which allows user to overwrite existing configurations. The default behavior is appending test cases on the existing configurations.
+- Support markdown report generation: The `test` command can generate markdown report when it found bugs for each test cases.
+
+## v0.1.0
+Initial release.
--- resource/data_source.go
+++ resource/data_source.go
@@ -3,28 +3,29 @@ package resource
 import (
 	"encoding/json"
 	"fmt"
-	"log"
 	"os"
 
-	pluralize "github.com/gertd/go-pluralize"
 	"github.com/ms-henglu/armstrong/hcl"
 	"github.com/ms-henglu/armstrong/resource/utils"
 	"github.com/ms-henglu/armstrong/types"
 )
 
-var _ Base = &Resource{}
+var _ Base = &DataSource{}
 
-var pluralizeClient = pluralize.NewClient()
-
-type Resource struct {
+type DataSource struct {
 	ApiVersion                 string
 	ExampleId                  string
-	ExampleBody                interface{}
 	PropertyDependencyMappings []PropertyDependencyMapping
 	Label                      string
 }
 
-func NewResourceFromExample(filepath string) (*Resource, error) {
+func (r *DataSource) GenerateLabel(references []Reference) string {
+	resourceType := utils.GetResourceType(r.ExampleId)
+	r.Label = newLabel(resourceType, references)
+	return r.Label
+}
+
+func NewDataSourceFromExample(filepath string) (*DataSource, error) {
 	exampleData, err := os.ReadFile(filepath)
 	if err != nil {
 		return nil, err
@@ -35,20 +36,12 @@ func NewResourceFromExample(filepath string) (*Resource, error) {
 		return nil, err
 	}
 
-	var body interface{}
 	exampleId := ""
 	apiVersion := ""
 	mappings := make([]PropertyDependencyMapping, 0)
 	if exampleMap, ok := example.(map[string]interface{}); ok {
 		if exampleMap["parameters"] != nil {
 			if parametersMap, ok := exampleMap["parameters"].(map[string]interface{}); ok {
-				for _, value := range parametersMap {
-					if bodyMap, ok := value.(map[string]interface{}); ok {
-						body = bodyMap
-					}
-				}
-				mappings = append(mappings, GetKeyValueMappings(body, "")...)
-
 				apiVersion = parametersMap["api-version"].(string)
 			}
 		}
@@ -73,74 +66,37 @@ func NewResourceFromExample(filepath string) (*Resource, error) {
 		}
 	}
 
-	return &Resource{
+	return &DataSource{
 		ApiVersion:                 apiVersion,
 		ExampleId:                  exampleId,
-		ExampleBody:                body,
 		PropertyDependencyMappings: mappings,
-		Label:                      "resource",
+		Label:                      "",
 	}, nil
 }
 
-func (r *Resource) RequiredDependencies(existingDependencies []types.Dependency, dependencies []types.Dependency) []types.Dependency {
+func (r *DataSource) RequiredDependencies(existingDependencies []types.Dependency, dependencies []types.Dependency) []types.Dependency {
 	return requiredDependencies(r.PropertyDependencyMappings, existingDependencies, dependencies)
 }
 
-func (r *Resource) UpdatePropertyDependencyMappingsReference(dependencies []types.Dependency, references []Reference) {
+func (r *DataSource) UpdatePropertyDependencyMappingsReference(dependencies []types.Dependency, references []Reference) {
 	r.PropertyDependencyMappings = updatePropertyDependencyMappingsReference(r.PropertyDependencyMappings, dependencies, references)
 }
 
-func (r *Resource) GenerateLabel(references []Reference) string {
+func (r *DataSource) Hcl(_ bool) string {
 	resourceType := utils.GetResourceType(r.ExampleId)
-	r.Label = newLabel(resourceType, references)
-	return r.Label
-}
-
-func (r *Resource) Hcl(useRawJsonPayload bool) string {
-	body := ""
-	if useRawJsonPayload {
-		jsonBody, _ := json.MarshalIndent(r.GetBody(), "", "    ")
-		body = fmt.Sprintf(`<<BODY
-%s
-BODY`, jsonBody)
-	} else {
-		hclBody := hcl.MarshalIndent(r.GetBody(), "", "  ")
-		body = fmt.Sprintf(`jsonencode(%s)`, hclBody)
+	name := utils.GetName(r.ExampleId)
+	if name != "default" {
+		name = hcl.RandomName()
 	}
-	resourceType := utils.GetResourceType(r.ExampleId)
 	parentId := FindParentReference(r.PropertyDependencyMappings)
 	if parentId == "" {
 		parentId = fmt.Sprintf(`"%s"`, utils.GetParentIdFromId(r.ExampleId))
 	}
 	return fmt.Sprintf(`
-resource "azapi_resource" "%[6]s" {
+data "azapi_resource" "%[5]s" {
 	type = "%[3]s@%[4]s"
 	parent_id = %[2]s
     name = "%[1]s"
-
- 	body = %[5]s
-
-    schema_validation_enabled = false
-    ignore_missing_property = false
 }
-`, hcl.RandomName(), parentId, resourceType, r.ApiVersion, body, r.Label)
-}
-
-func (r *Resource) GetBody() interface{} {
-	replacements := make(map[string]string)
-	for _, mapping := range r.PropertyDependencyMappings {
-		if mapping.ValuePath != "parent" && mapping.Reference != nil {
-			if mapping.Reference.IsKnown() {
-				valuePath := mapping.ValuePath
-				if mapping.IsKey {
-					valuePath = fmt.Sprintf("key:%s", mapping.ValuePath)
-				}
-				replacements[valuePath] = fmt.Sprintf(`${%s}`, mapping.Reference)
-			} else {
-				log.Printf("[WARN] reference is unkown, reference: %v", mapping.Reference)
-			}
-		}
-	}
-	removes := []string{".name"}
-	return utils.GetUpdatedBody(r.ExampleBody, replacements, removes, "")
+`, name, parentId, resourceType, r.ApiVersion, r.Label)
 }
