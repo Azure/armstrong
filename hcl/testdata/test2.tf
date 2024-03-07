d0287a24cead63631cefd2c660034b13a4e18f9d
teowa <104055472+teowa@users.noreply.github.com>
2024-03-07 17:27:30 +0800

    fix cred scan

---
+++ hcl/testdata/test2.tf
@@ -0,0 +1,32 @@
+resource "azurerm_resource_group" "test" {
+  name     = "acctest-rg"
+  location = "East US"
+}
+
+resource "azurerm_storage_account" "test" {
+  name                     = "acctestsa37"
+  resource_group_name      = azurerm_resource_group.test.name
+  location                 = azurerm_resource_group.test.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+}
+
+resource "azurerm_spring_cloud_service" "test" {
+  name                = "acctest-sc-37"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+}
+
+resource "azapi_resource" "test" {
+  type      = "Microsoft.AppPlatform/spring/storages@2024-01-01-preview"
+  name      = "acctest-ss-37"
+  parent_id = azurerm_spring_cloud_service.test.id
+
+  body = jsonencode({
+    properties = {
+      accountKey  = azurerm_storage_account.test.primary_access_key
+      accountName = azurerm_storage_account.test.name
+      storageType = "StorageAccount"
+    }
+  })
+}
